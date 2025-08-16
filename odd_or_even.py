7d70a35f8fbb61dafc487bacdc8b2798e5a25712
keiarmin <k.armin.ardeshiri@gmail.com>
2025-08-17 00:57:19 +0330

    Update odd_or_even.py

--- odd_or_even.py
+++ odd_or_even.py
@@ -1,4 +1,4 @@
-number input("Please enter a number and i will tell you if its Even or odd: ")
+number = input("Please enter a number and i will tell you if its Even or odd: ")
 if number % 2 == 0:
     print("your number is even")
 else:
