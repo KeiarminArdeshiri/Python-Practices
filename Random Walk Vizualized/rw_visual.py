25914f306e7408167e8d924c31aad4ffd2e8c26b
keiarmin <k.armin.ardeshiri@gmail.com>
2025-09-11 14:48:01 +0330

    Add files via upload

---
+++ Random Walk Vizualized/rw_visual.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+from random_walk import Randomwalk
+
+rw = Randomwalk()
+rw.fill_walk()
+
+plt.style.use('classic')
+fig, ax = plt.subplots()
+
+ax.scatter(rw.x_values, rw.y_values, s= 15)
+ax.set_aspect("equal")
+plt.show()
